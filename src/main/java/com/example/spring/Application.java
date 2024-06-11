4feef171b0de8ae7aca9216b8c3de30cfc402dde
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-06-11 20:41:50 +0300

    add fixes

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(Application.class, args);
     }
 
