a01d6271984ff075fcd74ee3a95a4b900d604510
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-06-24 19:44:14 +0300

    fixed checkstyle issues

--- src/main/java/com/example/spring/service/BookService.java
+++ src/main/java/com/example/spring/service/BookService.java
@@ -3,9 +3,8 @@ package com.example.spring.service;
 import com.example.spring.dto.BookDto;
 import com.example.spring.dto.BookSearchParameters;
 import com.example.spring.dto.CreateBookRequestDto;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
