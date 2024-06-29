02bc2bdaed9c335afdfade0a942bd179aa156397
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-06-29 06:56:21 +0300

    change user dto field max length

--- src/main/java/com/example/spring/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/com/example/spring/dto/user/UserRegistrationRequestDto.java
@@ -13,15 +13,18 @@ import org.hibernate.validator.constraints.Length;
 public class UserRegistrationRequestDto {
     @Email
     private String email;
-    @Length(min = 8)
+    @Length(min = 8, max = 255)
     @NotBlank
     private String password;
     @NotBlank
-    @Length(min = 8)
+    @Length(min = 8, max = 255)
     private String repeatPassword;
+    @Length(max = 255)
     @NotBlank
     private String firstName;
+    @Length(max = 255)
     @NotBlank
     private String lastName;
+    @Length(max = 255)
     private String shippingAddress;
 }
