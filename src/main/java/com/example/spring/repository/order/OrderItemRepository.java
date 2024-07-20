7db27e65e91e71245006e95dd5925742b30b0892
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-07-20 20:45:06 +0300

    add fixes after review

--- src/main/java/com/example/spring/repository/order/OrderItemRepository.java
+++ src/main/java/com/example/spring/repository/order/OrderItemRepository.java
@@ -4,9 +4,7 @@ import com.example.spring.model.OrderItem;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     @EntityGraph(attributePaths = {"book", "order"})
     Optional<OrderItem> findByIdAndOrderIdAndOrder_User_Id(Long itemId, Long orderId,Long userId);
--- src/main/java/com/example/spring/repository/order/OrderRepository.java
+++ src/main/java/com/example/spring/repository/order/OrderRepository.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @EntityGraph(attributePaths = {"orderItems"})
     List<Order> findAllByUserId(Long userId);
